f3492b2d46c3d59fe4e2cc8c966f6bb8ab7e482f
takanori-0701 <noriko-takahashi@grupside.com>
2024-12-29 16:55:07 +0900

    20241229

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 
+// ハンバーガーメニューとトップスライド
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
     $(function () {
         // ハンバーガーメニューの開閉
@@ -28,4 +29,93 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
             },
         });
     });
+});
+
+
+// 戻るボタン
+jQuery(function() {
+    var pagetop = $('#page_top');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
+    });
+});
+
+// 画像用アニメーション
+// アニメーション用の初期設定
+$(function () {
+    var box = $('.colorbox'),
+        speed = 700; // アニメーション速度
+
+    // 初期設定
+    box.each(function () {
+        $(this).append('<div class="color"></div>'); // 背景要素を追加
+        var color = $(this).find('.color'),
+            image = $(this).find('img, .colorbox__img');
+
+        image.css('opacity', '0'); // 画像を透明化
+        color.css('width', '0%'); // 背景の幅を0%に設定
+    });
+
+    // Intersection Observerで画面内に入ったらアニメーションを実行
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const colorBox = $(entry.target); // 対象のcolorboxを取得
+                const color = colorBox.find('.color');
+                const image = colorBox.find('img, .colorbox__img');
+
+                color.stop().animate(
+                    { width: '100%' },
+                    speed,
+                    function () {
+                        image.css('opacity', '1'); // 画像を表示
+                        $(this).css({ left: '0', right: 'auto' });
+                        $(this).animate({ width: '0%' }, speed); // 背景色を縮小
+                    }
+                );
+
+                // 一度だけアニメーションさせる
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    // colorboxを監視対象に設定
+    box.each(function () {
+        observer.observe(this);
+    });
+});
+
+ // campaign-swiper
+var campaign__swiper = new Swiper(".campaign-swiper", {
+    loop: true,
+    speed: 2000,
+    slidesPerView: "auto",
+    spaceBetween: 26,
+    autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+    },
+    watchOverflow: true, // スライドが1つの場合にも正しく動作する
+    breakpoints: {
+    768: {
+        slidesPerView: "auto",
+        spaceBetween: 40
+    }
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+    }
 });
